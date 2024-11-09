b55d925470b30d7692462518882db9867ad1bd8f
Karina Khaltunen <khaltunen@bk.ru>
2024-11-09 23:30:50 +0300

    Start priject

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -1,11 +1,9 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TodosTest {
     @Test
-    public void shouldAddThreeTasksOfDifferentType() {
+    public boolean shouldAddThreeTasksOfDifferentType() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
         String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
@@ -27,6 +25,7 @@ class TodosTest {
         Task[] expected = {simpleTask, epic, meeting};
         Task[] actual = todos.findAll();
         Assertions.assertArrayEquals(expected, actual);
+        return false;
     }
 
     @Test
@@ -184,30 +183,6 @@ class TodosTest {
 
     @Test
     public void FoundTasksZero() {
-        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
-
-        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
-        Epic epic = new Epic(55, subtasks);
-
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        Todos todos = new Todos();
-
-        todos.add(simpleTask);
-        todos.add(epic);
-        todos.add(meeting);
-
-        Task[] expected = {};
-        Task[] actual = todos.search("обед");
-        Assertions.assertArrayEquals(expected, actual);
-    }
-    @Test
-    public void FoundTasksZeroH() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
         String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
